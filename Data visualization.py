5c7022d8faf46b1f854c1f7d567247b367210d3d
Thanhhoa420 <23133022@student.hcmute.edu.vn>
2024-11-16 08:46:41 +0700

    Update Data visualization.py

--- Data visualization.py
+++ Data visualization.py
@@ -1,5 +1,26 @@
 import matplotlib.pyplot as plt
 
+class VideoGameBarChart:
+    def __init__(self, genres, counts):
+        self.genres = genres
+        self.counts = counts
+
+    def plot_chart(self):
+        plt.figure(figsize=(12, 6))
+        plt.bar(self.genres, self.counts, color='blue')
+
+        plt.title("Total Video Games by Genre", fontsize=16)
+        plt.xlabel("Genre", fontsize=12)
+        plt.ylabel("Count of Video Games", fontsize=12)
+
+        plt.xticks(rotation=45)
+
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+        plt.tight_layout()
+
+        plt.show()
+
 genres = [
     "Action", "Adventure", "Fighting", "Misc", "Platform", 
     "Puzzle", "Racing", "Role-Playing", "Shooter", 
@@ -7,15 +28,5 @@ genres = [
 ]
 counts = [3200, 1000, 700, 1700, 800, 500, 1100, 1500, 1400, 900, 1800, 600]
 
-plt.figure(figsize=(12, 6))
-plt.bar(genres, counts, color='blue')
-
-plt.title("Total Video Games by Genre", fontsize=16)
-plt.xlabel("Genre", fontsize=12)
-plt.ylabel("Count of Video Games", fontsize=12)
-
-plt.xticks(rotation=45)
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-
-plt.tight_layout()
-plt.show()
+chart = VideoGameBarChart(genres, counts)
+chart.plot_chart()
