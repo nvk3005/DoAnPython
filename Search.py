3fe58d95b2f6153b2353da7eed7cf1cd10d64a9b
Nguyễn Văn Kế <157278949+nvk3005@users.noreply.github.com>
2024-12-07 23:56:27 +0700

    Add files via upload

--- Search.py
+++ Search.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-class Search:
+class Search_Class:
+    '''Lớp thực hiện chức năng tìm kiếm trên cột'''
     def __init__(self, data_file):
         self.data_file = data_file
     
-    def search_by_column(self, column_name, value_search):
+    def search_by_column(self, column_name: str, value_search):
         try:
             data = pd.read_csv(self.data_file)
         except Exception as e:
             return f"Lỗi đọc File: {e}"
-        
-        if column_name not in data.columns:
-            return f"Tên cột '{column_name}' không có trong dataset."
-        
-        filtered_data = data[data[column_name] == value_search]
-        return filtered_data
+
+        search_data = data[data[column_name] == value_search]
+        if not search_data.empty:
+            return search_data
+        else:
+            return "Không tìm thấy dữ liệu trong phù hợp."
